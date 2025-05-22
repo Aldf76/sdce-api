9da5580b1952ba8d7c93f6e7e1ab1fdbe00ba724
Aldf76 <contasfelipe623@gmail.com>
2025-05-22 18:57:24 -0300

    Projeto inicial SGCE com SpringBoot

---
+++ src/test/java/com/sgce/sgce_api/SgceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgce.sgce_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
